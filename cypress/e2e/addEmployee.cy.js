6b890019ba7c164f5fa65d5f390cd259910d8181
Ekanaradhipa Djacaria <ekanaradhipad@gmail.com>
2026-09-16 12:06:21 +0700

    add employee

--- cypress/e2e/addEmployee.cy.js
+++ cypress/e2e/addEmployee.cy.js
@@ -13,15 +13,40 @@ describe('OrangeHRM employee management', () => {
         .should('be.enabled')
         .click();
 
-      cy.visit('/web/index.php/admin/viewSystemUsers');
-    //  cy.contains('h6', 'System Users').should('be.visible');
+      cy.visit('/web/index.php/pim/addEmployee');
+
+      cy.get('input[name="firstName"]')
+        .should('exist')
+        .and('be.visible')
+        .type('firstName');
+
+      cy.get('input[name="middleName"]')
+        .should('exist')
+        .and('be.visible')
+        .type('middleName');
+
+      cy.get('input[name="lastName"]')
+        .should('exist')
+        .and('be.visible')
+        .type('lastName');
+
+      cy.get('button[type="submit"]')
+        .should('be.enabled')
+        .click();
+
+      cy.contains('h6', 'Personal Details', { timeout: 15000 })
+        .should('be.visible');
+
+      cy.url().should('include', '/web/index.php/pim/viewPersonalDetails/empNumber/');
+
+    /*  cy.contains('h6', 'System Users').should('be.visible');
       cy.contains('button', 'Add').click();
 
       cy.url().should('include', '/web/index.php/admin/saveSystemUser');
       cy.get('label').contains('User Role').parent().next().find('.oxd-select-text').click();
       cy.contains('.oxd-select-option', 'ESS').click();
 
-      cy.get('label').contains('Employee Name').parent().next().find('input').type('a');
+      cy.get('label').contains('Employee Name').parent().next().find('input').type('Employee Test');
       cy.get('.oxd-autocomplete-option').first().click();
 
       cy.get('label').contains('Status').parent().next().find('.oxd-select-text').click();
@@ -33,7 +58,7 @@ describe('OrangeHRM employee management', () => {
 
       cy.contains('button', 'Save').click();
       cy.contains('.oxd-toast', 'Successfully Saved').should('be.visible');
-
+      */
     });
   });
 });
